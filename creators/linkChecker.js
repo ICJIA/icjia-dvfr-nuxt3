0dea7a6b4b5d68e620d73ea01d2ee6eefb358940
cschweda <cschweda@gmail.com>
2023-02-15 17:46:50 -0600

    fix: test link checker

--- creators/linkChecker.js
+++ creators/linkChecker.js
@@ -17,3 +17,25 @@ links.forEach((link) => {
     console.log(link, result);
   });
 });
+
+// remove spaces from files in a directory
+
+// const fs = require("fs");
+// const path = require("path");
+
+// const directoryPath = path.join(__dirname, "test");
+
+// fs.readdir(directoryPath, (err, files) => {
+//   if (err) {
+//     return console.log("Unable to scan directory: " + err);
+//   }
+
+//   files.forEach((file) => {
+//     const newFile = file.replace(/ /g, "");
+//     fs.renameSync(
+//       path.join(directoryPath, file),
+//       path.join(directoryPath, newFile)
+//     );
+//     console.log("Renaming " + file + " to " + newFile);
+//   });
+// });
