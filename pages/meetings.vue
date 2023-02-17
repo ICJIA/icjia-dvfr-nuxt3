f28247bbc232ff186808ea851ccc6b26131fb4ab
cschweda <cschweda@gmail.com>
2023-02-17 15:08:20 -0600

    fix: byte display in attachment table

--- pages/meetings.vue
+++ pages/meetings.vue
@@ -19,6 +19,40 @@ const formatDate = (dateString) => {
   return new Date(dateString).toLocaleDateString(undefined, options);
 };
 
+// const units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+// const niceBytes = (x) => {
+//   let l = 0,
+//     n = parseInt(x, 10) || 0;
+//   while (n >= 1024 && ++l) {
+//     n = n / 1024;
+//   }
+//   return n.toFixed(n < 10 && l > 0 ? 1 : 0) + " " + units[l];
+// };
+
+const niceBytes = (bytes, si = false, dp = 1) => {
+  const thresh = si ? 1000 : 1024;
+
+  if (Math.abs(bytes) < thresh) {
+    return bytes + " B";
+  }
+
+  const units = si
+    ? ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+  let u = -1;
+  const r = 10 ** dp;
+
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (
+    Math.round(Math.abs(bytes) * r) / r >= thresh &&
+    u < units.length - 1
+  );
+
+  return bytes.toFixed(dp) + " " + units[u];
+};
+
 useHead({
   meta: [
     {
@@ -128,13 +162,18 @@ useHead({
                             </td>
                             <td>
                               {{
-                                meeting.attachments.data[index].attributes
-                                  .updatedAt
+                                formatDate(
+                                  meeting.attachments.data[index].attributes
+                                    .updatedAt
+                                )
                               }}
                             </td>
                             <td>
                               {{
-                                meeting.attachments.data[index].attributes.size
+                                niceBytes(
+                                  meeting.attachments.data[index].attributes
+                                    .size
+                                )
                               }}
                             </td>
                           </tr>
