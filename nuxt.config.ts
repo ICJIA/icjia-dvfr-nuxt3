e8d57f1cd66e19ed372de8d78cf68f2b89126f2c
cschweda <cschweda@gmail.com>
2023-06-26 12:37:30 -0500

    fix: DoIt analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,6 +42,11 @@ export default defineNuxtConfig({
           src: "https://code.jquery.com/jquery-3.6.1.slim.min.js",
           type: "text/javascript",
         },
+         {
+          src: "https://assets.adobedtm.com/c318d2739692/96e37aff7009/launch-4ef36d3c8aed.min.js",
+          type: "text/javascript",
+          async: true
+        },
         {
           src: "https://analytics.icjia-api.cloud/js/plausible.js",
           "data-domain": "dvfr.illinois.gov",
